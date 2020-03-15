0d211ae74a45cf3ff767efc49fb2067d3fc36e06
OdSazib <odsazib@gmail.com>
2020-03-15 22:50:09 +0600

    sdm660-common: Remove snap config

--- common_prop.mk
+++ common_prop.mk
@@ -116,12 +116,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.privapp.list=org.codeaurora.snapcam \
     persist.vendor.camera.dual.isp.sync=0 \
     persist.vendor.camera.HAL3.enabled=1 \
     persist.vendor.camera.eis.enable=1 \
     persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
     vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord
 
 # Codec2 switch
